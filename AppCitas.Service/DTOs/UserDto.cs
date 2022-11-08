8702c22951dbad2d8128a1ce2de39985380afb6a
OmarMuro18 <omarmuro117@gmail.com>
2022-11-08 08:33:06 -0600

    Updating the API register method

--- AppCitas.Service/DTOs/UserDto.cs
+++ AppCitas.Service/DTOs/UserDto.cs
@@ -5,4 +5,5 @@ public class UserDto
     public string Username { get; set; }
     public string Token { get; set; }
     public string PhotoUr1 { get; set; }
+    public string KnownAs { get; set; }
 }
