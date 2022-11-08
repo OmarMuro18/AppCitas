68439deed3b5ef14038cfcfe452de16718c7dafc
OmarMuro18 <omarmuro117@gmail.com>
2022-11-08 09:52:30 -0600

    Using the pagination classes

--- AppCitas.Service/Extensions/HttpExtensions.cs
+++ AppCitas.Service/Extensions/HttpExtensions.cs
@@ -9,6 +9,10 @@ public static class HttpExtensions
         int itemsPerPage, int totalItems, int totalPages)
     {
         var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
         response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader));
         response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
     }
